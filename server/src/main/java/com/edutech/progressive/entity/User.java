fc2b893df089cc57052c163152b9c47a9bfc5b54
tanvipawar27 <dummy@gmail.com>
2026-02-23 11:28:30 +0000

    test

--- server/src/main/java/com/edutech/progressive/entity/User.java
+++ server/src/main/java/com/edutech/progressive/entity/User.java
@@ -1,5 +1,61 @@
 package com.edutech.progressive.entity;
 
+import javax.persistence.Entity;
+
+@Entity
 public class User {
+
+    private int userId;
+    private String fullName;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    public User() {
+    }
+    public User(int userId, String fullName, String username, String password, String email, String role) {
+        this.userId = userId;
+        this.fullName = fullName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }   
 
 }
