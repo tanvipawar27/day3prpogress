51f76b691dbdbe9e44861643b05b7a6c0e1cd9e6
Tejas Zope <148805291+Tejas499@users.noreply.github.com>
2026-02-24 10:16:55 +0530

    Update CricketerServiceImplJdbc.java

--- server/src/main/java/com/edutech/progressive/service/impl/CricketerServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CricketerServiceImplJdbc.java
@@ -1,15 +1,13 @@
 package com.edutech.progressive.service.impl;
 
-
 import com.edutech.progressive.dao.CricketerDAO;
 import com.edutech.progressive.entity.Cricketer;
 import com.edutech.progressive.service.CricketerService;
-
-import java.util.ArrayList;
+import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 
 public class CricketerServiceImplJdbc implements CricketerService {
-
     private CricketerDAO cricketerDAO;
 
     public CricketerServiceImplJdbc(CricketerDAO cricketerDAO) {
@@ -17,30 +15,38 @@ public class CricketerServiceImplJdbc implements CricketerService {
     }
 
     @Override
-    public List<Cricketer> getAllCricketers() {
-        return new ArrayList<>();
+    public List<Cricketer> getAllCricketers() throws SQLException {
+        return cricketerDAO.getAllCricketers();
     }
 
     @Override
-    public Integer addCricketer(Cricketer cricketer) {
-        return -1;
+    public Integer addCricketer(Cricketer cricketer) throws SQLException {
+        int id = cricketerDAO.addCricketer(cricketer);
+        if (id > 0) {
+            cricketer.setCricketerId(id);
+        }
+        return id;
     }
 
     @Override
-    public List<Cricketer> getAllCricketersSortedByExperience() {
-        return new ArrayList<>();
+    public List<Cricketer> getAllCricketersSortedByExperience() throws SQLException {
+        List<Cricketer> list = cricketerDAO.getAllCricketers();
+        Collections.sort(list);
+        return list;
     }
 
     @Override
-    public void updateCricketer(Cricketer cricketer) {
+    public void updateCricketer(Cricketer cricketer) throws SQLException {
+        cricketerDAO.updateCricketer(cricketer);
     }
 
     @Override
-    public void deleteCricketer(int cricketerId) {
+    public void deleteCricketer(int cricketerId) throws SQLException {
+        cricketerDAO.deleteCricketer(cricketerId);
     }
 
     @Override
-    public Cricketer getCricketerById(int cricketerId) {
-        return null;
+    public Cricketer getCricketerById(int cricketerId) throws SQLException {
+        return cricketerDAO.getCricketerById(cricketerId);
     }
 }
