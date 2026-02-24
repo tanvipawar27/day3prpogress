825036f80b61ad1059b151c1cab27d5fc65ac309
Tejas Zope <148805291+Tejas499@users.noreply.github.com>
2026-02-24 10:18:11 +0530

    Update MatchServiceImplJdbc.java

--- server/src/main/java/com/edutech/progressive/service/impl/MatchServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/MatchServiceImplJdbc.java
@@ -1,15 +1,12 @@
 package com.edutech.progressive.service.impl;
 
-import com.edutech.progressive. dao.MatchDAO;
+import com.edutech.progressive.dao.MatchDAO;
 import com.edutech.progressive.entity.Match;
 import com.edutech.progressive.service.MatchService;
-
-import com.edutech.progressive.entity.*;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
 
 public class MatchServiceImplJdbc implements MatchService {
-
     private MatchDAO matchDAO;
 
     public MatchServiceImplJdbc(MatchDAO matchDAO) {
@@ -17,25 +14,31 @@ public class MatchServiceImplJdbc implements MatchService {
     }
 
     @Override
-    public List<Match> getAllMatches() {
-        return new ArrayList<>();
+    public List<Match> getAllMatches() throws SQLException {
+        return matchDAO.getAllMatches();
     }
 
     @Override
-    public Match getMatchById(int matchId) {
-        return null;
+    public Match getMatchById(int matchId) throws SQLException {
+        return matchDAO.getMatchById(matchId);
     }
 
     @Override
-    public Integer addMatch(Match match) {
-        return -1;
+    public Integer addMatch(Match match) throws SQLException {
+        int id = matchDAO.addMatch(match);
+        if (id > 0) {
+            match.setMatchId(id);
+        }
+        return id;
     }
 
     @Override
-    public void updateMatch(Match match) {
+    public void updateMatch(Match match) throws SQLException {
+        matchDAO.updateMatch(match);
     }
 
     @Override
-    public void deleteMatch(int matchId) {
+    public void deleteMatch(int matchId) throws SQLException {
+        matchDAO.deleteMatch(matchId);
     }
-} 
+}
